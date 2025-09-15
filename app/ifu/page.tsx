37d330fb0549f8045c026f666c8d69694748e232
mort-next <kaskaskas2022@gmail.com>
2025-09-15 10:41:48 +0900

    説明画面の一部画像挿入、ホームへのリンク追加

--- app/ifu/page.tsx
+++ app/ifu/page.tsx
@@ -4,13 +4,16 @@ import * as React from 'react';
 import { styled, alpha, ThemeProvider, createTheme } from '@mui/material/styles';
 import Link from "next/link";
 import { Search, Plus , HelpCircle} from "lucide-react";
+import Image from 'next/image';
 
 export default function Home() {
   return (
       <>
        {/* ヘッダー */}
        <header className="w-full flex justify-between items-center bg-blue-600 text-white px-4 py-3 shadow">
+        <a href="http://localhost:3000/">
         <h1 className="text-lg font-bold">北九log</h1>
+        </a>
         <Link
         href="/ifu"
         className="bg-blue-500 text-white p-4 rounded-full shadow-lg hover:bg-blue-700"
@@ -39,9 +42,13 @@ export default function Home() {
             <p>
                 2. 下にあるように、画像、タグ、コメント、URL(任意)を入力し、投稿ボタンを押すことで投稿できます。
             </p>
-            <p>
-                image
-            </p>
+            <img
+            src="/ifu_2.png"
+            alt="投稿画面"
+            width={400}
+            height={200}
+            style={{ width: '40%', height: 'auto' }}
+            />
         <p className="text-3xl font-bold">
             閲覧方法
         </p>
@@ -54,9 +61,13 @@ export default function Home() {
             <p>
                 2. 検索画面では、タグ、場所(何区か)を選択することで、検索が出来ます。
             </p>
-            <p>
-                image
-            </p>
+            <img
+            src="/ifu_4.png"
+            alt="検索画面"
+            width={400}
+            height={200}
+            style={{ width: '40%', height: 'auto' }}
+            />
             <p>
                 3. 検索結果やホーム画面で、投稿をクリック(タッチ)することで閲覧できます。
             </p>
